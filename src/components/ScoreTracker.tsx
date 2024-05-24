5a16f85ef701739e1db726615ceeb813252eb0ed
Ryan Christie <ryanchristie@Ryans-MacBook-Pro.local>
2024-05-24 18:17:36 -0400

    added score counter component

--- src/components/ScoreTracker.tsx
+++ src/components/ScoreTracker.tsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux"
 import { RootState } from "../state/store"
+import "./ScoreTracker.scss"
 
 const ScoreTracker = () => {
   const winCount = useSelector((state: RootState) => state.winCounterReducer)
 
   return (
-    <div>
-      <div>User Wins: {winCount.user}</div>
+    <div className="score-container">
       <div>AI Wins: {winCount.ai}</div>
+      <div>User Wins: {winCount.user}</div>
     </div>
   )
 }
